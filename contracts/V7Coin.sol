605d7c57b3e8bf86dbd0dc4853f758b2c3a98467
Dmitriy <voytenko.d@ideasoft.io>
2021-08-02 18:51:28 +0300

    Cleanup environment.
    Added V7Coin contract.
    
    Added process env variable.
    
    Updated erc20 instants.

--- contracts/V7Coin.sol
+++ contracts/V7Coin.sol
@@ -1,9 +1,12 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
 
+pragma solidity >=0.8.0 <0.9.0;
 import "./token/ERC20/ERC20.sol";
 
-contract V7 is ERC20 {
-    constructor() ERC20("Decentralized Systems", "V7") {
-        _mint(msg.sender, 1000);
+contract V7Coin is ERC20 {
+    address public owner = msg.sender;
+
+    constructor() ERC20("V7Coin", "V7", 18, owner) {
+        _mint(msg.sender, 57896044);
     }
 }
